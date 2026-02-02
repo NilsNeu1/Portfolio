1d736c5fc15ee6632c3b3b62a8eac7a75f29480a
Nils <schoenfeld_nils@gmx.de>
2026-02-02 19:36:06 +0100

    added Header Interctions, Language select, Burgermenu with animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Portfolio';
 }
+
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf } from "../../../node_modules/@angular/common/index";
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+isMenuOpen = false;
+activeLanguage = 'DE';
+langAnimate = false;
 
+  goToLink(url: string) {
+  window.open(url, '_blank');
+}
+
+
+burgerMenuOpen(){
+this.isMenuOpen = true;
+}
+
+burgerMenuClose(){
+this.isMenuOpen = false;
+}
+
+languageSwitch(){
+this.activeLanguage = this.activeLanguage === 'DE' ? 'EN' : 'DE';
+this.langAnimate = true;
+setTimeout(() => {
+  this.langAnimate = false;
+}, 300);
+console.log('active language is', this.activeLanguage)
+}
 
 }
