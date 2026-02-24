e20f3ab06b7c4c5fb4bc1d02c023b7671e11a575
Nils <schoenfeld_nils@gmx.de>
2026-02-24 23:07:43 +0100

    edited skill section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, NgForOf],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+
+  skills = [
+    {icon: '../../assets/Frontend_Skills_icons/HTML.svg', label: 'HTML' },
+    {icon: '../../assets/Frontend_Skills_icons/CSS.svg', label: 'CSS' },
+    {icon: '../../assets/Frontend_Skills_icons/Javascript.svg', label: 'JavaScript' },
+    {icon: '../../assets/Frontend_Skills_icons/Typescript.svg', label: 'TypeScript' },
+    {icon: '../../assets/Frontend_Skills_icons/Angular.svg', label: 'Angular' },
+    // {icon: '../../assets/Frontend_Skills_icons/Firebase.svg', label: 'Firebase' },
+    {icon: '../../assets/Frontend_Skills_icons/Git.svg', label: 'Git' },
+    {icon: '../../assets/Frontend_Skills_icons/Rest_Api.svg', label: 'REST-API' },
+    {icon: '../../assets/Frontend_Skills_icons/Material_Design.svg', label: 'Material Design' },
+    {icon: '../../assets/Frontend_Skills_icons/Scrum.svg', label: 'Scrum' },
+  ]
+
 
 }
