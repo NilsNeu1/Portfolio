cffd6f035616c6a55e6559121486beeb7f2a6780
Nils <schoenfeld_nils@gmx.de>
2026-03-03 21:11:22 +0100

    wip: my-work section

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ObserveSectionDirective } from '../services/section-observer/section.observer.directive';
+import { url } from 'node:inspector';
 
 @Component({
   selector: 'app-my-work',
@@ -10,5 +11,26 @@ import { ObserveSectionDirective } from '../services/section-observer/section.ob
 })
 export class MyWorkComponent {
   sectionId = 'projects';
+
+  projects = [
+    {
+      name: "El Pollo Loco",
+      icon: "../../assets/general/Project_emoji/Pollo_loco_D.svg",
+      snapshot: "",
+      technologies: ["HTML", "CSS", "Javascript"],
+      description: "A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to defeat his enemies and collect Coins",
+      github: "",
+      redirect:""
+    },
+    {
+      name: "Join",
+      icon: url(),
+      snapshot: "",
+      technologies: ["HTML", "CSS", "TypeScript", "Angular"],
+      description: "A Task-manager inspired by the Kanban System. ",
+      github: "",
+      redirect:""
+    },
+  ];
 
 }
